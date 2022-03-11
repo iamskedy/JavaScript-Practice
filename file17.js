7912bbfc561c3c7318512b25fec2b034aa775ad1
Shubham <shubhamkumar.dubey02@gmail.com>
2022-03-11 16:32:38 +0530

    update

--- file17.js
+++ file17.js
@@ -1,1 +1,23 @@
 // Nested IF else
+
+
+let winningNum=19;
+// let userGuess=prompt("Guess a number");//This is in the form of string
+// console.log(typeof userGuess,userGuess );
+
+
+let userGuess= +prompt("Guess a number");//This is in the form of Number
+console.log(typeof userGuess,userGuess );
+
+
+if (userGuess===winningNum)
+{ console.log("Your guess is right" );
+}
+else{
+    if(userGuess < winningNum)
+    {    console.log("too low !!!" );
+    }
+    else{
+        console.log("too high !!!" );
+        }
+}
---
+++ file18.js
@@ -0,0 +1,22 @@
+// if
+// else if
+// else if
+// else if
+// else
+
+
+let tempInDegree =+prompt("Temperature Outside");
+
+if(tempInDegree < 0 ){
+    console.log("Extremely cold outside");
+}else if(tempInDegree < 16 ){
+    console.log("It is cold outside");
+}else if(tempInDegree < 25 ){
+    console.log("weather is Okay");
+}else if(tempInDegree < 35 ){
+    console.log("lets go for swim");
+}else if(tempInDegree < 45 ){
+    console.log("Turn on AC");
+}else{
+    console.log("too hot!!");
+}
